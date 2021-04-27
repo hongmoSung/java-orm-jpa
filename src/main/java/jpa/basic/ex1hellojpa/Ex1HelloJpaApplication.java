f0c9539f1ab4bca57397a34b2721ff8f7d99fbd7
hongmo sung <tesung30@naver.com>
2021-04-27 19:26:24 +0900

    Connection fix

--- src/main/java/jpa/basic/ex1hellojpa/Ex1HelloJpaApplication.java
+++ src/main/java/jpa/basic/ex1hellojpa/Ex1HelloJpaApplication.java
@@ -1,13 +1,13 @@
-package jpa.basic.ex1hellojpa;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Ex1HelloJpaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Ex1HelloJpaApplication.class, args);
-	}
-
-}
+//package jpa.basic.ex1hellojpa;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class Ex1HelloJpaApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(Ex1HelloJpaApplication.class, args);
+//	}
+//
+//}
