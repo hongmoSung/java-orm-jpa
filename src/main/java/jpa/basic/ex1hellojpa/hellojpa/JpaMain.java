e7d4075ec190212322550532bf70d2a8b2ef7656
hongmo sung <tesung30@naver.com>
2021-04-27 21:10:32 +0900

    양방향 연관관계와 연관관계의 주인 1- 기본

--- src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -30,8 +31,11 @@ public class JpaMain {
             em.clear();
 
             final Member findMember = em.find(Member.class, member.getId());
-            final Team findTeam = findMember.getTeam();
-            System.out.println("findTeam -> " + findTeam.getName());
+            final List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m -> " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpa/basic/ex1hellojpa/hellojpa/Team.java
+++ src/main/java/jpa/basic/ex1hellojpa/hellojpa/Team.java
@@ -1,9 +1,8 @@
 package jpa.basic.ex1hellojpa.hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -13,6 +12,8 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>(); // 초기화 관계 add할때 널이 안뜸?
 
     public Long getId() {
         return id;
@@ -28,5 +29,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
