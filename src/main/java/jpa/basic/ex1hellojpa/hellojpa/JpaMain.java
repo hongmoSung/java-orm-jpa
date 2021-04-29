9e9cb8d0cbd00e559b1d5a79f43301f650dd1ff1
hongmo sung <tesung30@naver.com>
2021-04-29 21:02:56 +0900

    Mapped Superclass - 매핑 정보 상속

--- src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -15,6 +16,11 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setName("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
+            em.persist(member);
             tx.commit();
         } catch (Exception e) {
             System.out.println(e.getMessage());
