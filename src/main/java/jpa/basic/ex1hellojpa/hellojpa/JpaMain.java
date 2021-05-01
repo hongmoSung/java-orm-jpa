1213ffccecf4bbe9d5915a2b2611342d00a854c0
hongmo sung <tesung30@naver.com>
2021-05-01 19:28:07 +0900

    CASCADE

--- src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
@@ -1,12 +1,9 @@
 package jpa.basic.ex1hellojpa.hellojpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -18,34 +15,23 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            Team team2 = new Team();
-            team.setName("teamB");
 
-            em.persist(team);
-            em.persist(team2);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member = new Member();
-            member.setName("user1");
-            member.setTeam(team);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member2 = new Member();
-            member.setName("user2");
-            member.setTeam(team2);
-
-            em.persist(member);
-            em.persist(member2);
+//            em.persist(child1);
+//            em.persist(child2);
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-//            final Member m = em.find(Member.class, member.getId());
-            final List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
-                    .getResultList();
-
-        } catch (Exception e) {
             tx.commit();
+        } catch (Exception e) {
             e.printStackTrace();
             tx.rollback();
         } finally {
