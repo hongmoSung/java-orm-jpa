7724b1390a15c45c0845f122db5e9f9cdcbb04c1
hongmo sung <tesung30@naver.com>
2021-05-01 17:10:35 +0900

    프록시

--- src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
@@ -1,10 +1,11 @@
 package jpa.basic.ex1hellojpa.hellojpa;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -18,12 +19,23 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setName("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+//            final Member findMember = em.find(Member.class, member.getId());
+            final Member refMember = em.getReference(Member.class, member.getId());
+            System.out.println("refMember -> " + refMember.getClass());
+            System.out.println(emf.getPersistenceUnitUtil().isLoaded(refMember));
+
+//            em.detach(refMember);
+            Hibernate.initialize(refMember);
+            System.out.println(emf.getPersistenceUnitUtil().isLoaded(refMember));
+
             tx.commit();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
             tx.rollback();
         } finally {
             em.close();
