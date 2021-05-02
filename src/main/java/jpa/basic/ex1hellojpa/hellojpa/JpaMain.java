553609e48c20153a73bfad3cf146d6413f193abe
hongmo sung <tesung30@naver.com>
2021-05-02 16:51:10 +0900

    JPQL소개

--- src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/jpa/basic/ex1hellojpa/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -15,29 +16,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("서울특별시", "관악구", "9999"));
+            String jpql = "select m from Member m where m.username like '%member%'";
+            final List<Member> members = em.createQuery(jpql, Member.class)
+                    .getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressesHistory().add(new AddressEntity("경기도", "분당구", "1111"));
-            member.getAddressesHistory().add(new AddressEntity("부산", "남포동", "2222"));
-
-            em.persist(member);
-            System.out.println("================start==================");
-//            final Member findMember = em.find(Member.class, member.getId());
-//            final Address homeAddress = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("mewCity", homeAddress.getStreet(), homeAddress.getZipcode()));
-//
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-//
-//            findMember.getAddressesHistory().remove(new AddressEntity("부산", "남포동", "2222"));
-//            findMember.getAddressesHistory().add(new AddressEntity("newCity", "남포동", "2222"));
-
+            for (Member member : members) {
+                System.out.println("member -> " + member.getUsername());
+            }
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
