5d884a1b9223d0cd3041d6d2a820a246a3d49701
hongmo sung <tesung30@naver.com>
2021-05-02 17:39:06 +0900

    기본 문법과 쿼리 API

--- src/main/java/jpa/basic/ex1hellojpa/jpql/JpaMain.java
+++ src/main/java/jpa/basic/ex1hellojpa/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpa.basic.ex1hellojpa.jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JpaMain {
 
@@ -17,7 +14,13 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("홍성모");
+            member.setAge(10);
             em.persist(member);
+
+            final TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query.setParameter("username", "홍성모");
+            final Member result = query.getSingleResult();
+            System.out.println("singleResult -> " + result.getUsername());
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
