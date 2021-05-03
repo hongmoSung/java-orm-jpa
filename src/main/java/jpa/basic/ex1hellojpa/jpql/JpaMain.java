d98dd5e0a3be98bd555c4b4f82f6eb77412e50e6
hongmo sung <tesung30@naver.com>
2021-05-03 19:05:52 +0900

    패치 조인1 - 기본 n + 1 문제

--- src/main/java/jpa/basic/ex1hellojpa/jpql/JpaMain.java
+++ src/main/java/jpa/basic/ex1hellojpa/jpql/JpaMain.java
@@ -1,6 +1,10 @@
 package jpa.basic.ex1hellojpa.jpql;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -13,22 +17,46 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team team1 = new Team();
+            team1.setName("팀A");
+            em.persist(team1);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            Team team2 = new Team();
+            team2.setName("팀B");
+            em.persist(team2);
+
+            Team team3 = new Team();
+            team3.setName("팀C");
+            em.persist(team3);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(team1);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(team1);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(team2);
+            em.persist(member3);
+
+//            Member member4 = new Member();
+//            member4.setUsername("회원4");
+//            em.persist(member4);
 
             em.flush();
             em.clear();
 
-            String query = "select locate('de', 'asdf') from Member m";
-            em.createQuery(query, Integer.class).getResultList();
+            String query = "select m from Member m";
+            final List<Member> result = em.createQuery(query, Member.class).getResultList();
+
+            for (Member member : result) {
+                System.out.println("member => " + member.getUsername() + member.getTeam().getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
