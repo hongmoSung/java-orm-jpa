9d9af20096bfbdef743a3155769e9e379e4f3b79
hongmo sung <tesung30@naver.com>
2021-06-19 17:08:10 +0900

    복습

--- src/main/java/jpa/basic/ex1hellojpa/Ex1HelloJpaApplication.java
+++ src/main/java/jpa/basic/ex1hellojpa/Ex1HelloJpaApplication.java
@@ -1,13 +1,32 @@
-//package jpa.basic.ex1hellojpa;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class Ex1HelloJpaApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(Ex1HelloJpaApplication.class, args);
-//	}
-//
-//}
+package jpa.basic.ex1hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Ex1HelloJpaApplication {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            final List<Member> members = em.createQuery("select m from Member as m", Member.class).getResultList();
+            for (Member member : members) {
+                System.out.println("member -> " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
--- src/main/java/jpa/basic/ex1hellojpa/jpql/MemberType.java
+++
@@ -1,5 +0,0 @@
-package jpa.basic.ex1hellojpa.jpql;
-
-public enum MemberType {
-    ADMIN, USER
-}
