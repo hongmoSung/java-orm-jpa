18d4f9d638a2547908dfe737e5d1b0f593c4c91c
hongmo sung <tesung30@naver.com>
2021-12-21 17:30:42 +0900

    영속성 컨택스트2

--- src/main/java/jpa/basic/ex1hellojpa/jpql/JpaMain.java
+++ src/main/java/jpa/basic/ex1hellojpa/jpql/JpaMain.java
@@ -15,21 +15,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("member1");
+            final Member member = em.find(Member.class, 150L);
+            member.setName("ZZZ");
 
-            // 영속
-            System.out.println("========== Before ============");
-            em.persist(member);
-            System.out.println("========== After ============");
-
-            final Member findMember = em.find(Member.class, 101L);
-
-            System.out.println(findMember.getId());
-            System.out.println(findMember.getName());
-
+            System.out.println("=======================");
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/jpa/basic/ex1hellojpa/jpql/Member.java
+++ src/main/java/jpa/basic/ex1hellojpa/jpql/Member.java
@@ -1,26 +1,16 @@
 package jpa.basic.ex1hellojpa.jpql;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
